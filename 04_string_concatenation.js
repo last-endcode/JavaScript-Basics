ec552981749572190634e6e68b6dbbc7b13d750c
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-09 13:54:16 +0700

    string concatenation

---
+++ 04_string_concatenation.js
@@ -0,0 +1,16 @@
+// concatenation string is for combination
+// `` - template strings easier option
+
+// concatenation string
+const name = 'john';
+const lastName = 'doe';
+const fullName = name + ' ' + lastName;
+
+// console.log('your full name is : ' + name + ' ' + lastName);
+
+console.log('your full name is : ' + fullName);
+
+// example again
+const website = 'freecodecamp';
+const url = 'https://' + website + '.com';
+console.log(url);
