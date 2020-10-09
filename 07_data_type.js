d7a9ebae5ae5851e5dbeb7909d0619f10a1973bd
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-09 18:43:12 +0700

    data types

---
+++ 07_data_type.js
@@ -0,0 +1,25 @@
+/* data type on js totaly is 7 
+Primitive - string, number,bootlean, null, undefined,
+symbol,
+Object - arrays, functions, objects
+typeof - operator (typeof variable) (typeof value)
+String
+*/
+
+// string
+let name = 'dncode';
+// number
+const amount = 1000;
+// boolean
+const booleanValue = false;
+// null
+const data = null;
+// undefined
+let urlink;
+const empty = 0;
+console.log(typeof name);
+console.log(typeof amount);
+console.log(typeof booleanValue);
+console.log(typeof data); /*null will result object*/
+console.log(typeof urlink);
+console.log(typeof empty);
