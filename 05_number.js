46b48370077765cda3b8fced90ed88b895ffdac7
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-09 14:06:45 +0700

    number basic

---
+++ 05_number.js
@@ -0,0 +1,24 @@
+// number
+
+const num1 = 35.46;
+const num2 = 45.52;
+const num3 = '45.665';
+
+// here on console vcode values is orange color
+console.log(num1);
+console.log(num2);
+// but for string num3  is white color for string.
+console.log(num3);
+
+// example again math basic
+console.log();
+console.log('basic math opeartor');
+const add = num1 + num2;
+const sub = num1 - num2;
+const mult = num1 * num2;
+const div = num1 / num2;
+
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
