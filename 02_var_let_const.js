79f5ecc193f43496b79dda78ca2004f773f02061
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-09 13:33:32 +0700

    var let const

---
+++ 02_var_let_const.js
@@ -0,0 +1,14 @@
+// var, let , const
+
+var name = 'john';
+name = 'dncode';
+let lastname = 'peter';
+lastname = 'darkcode';
+
+// const (constant) can't re-assign
+const fullName = 'john doe';
+fullName = 'peter doe';
+
+console.log(name);
+console.log(lastname);
+console.log(fullName); /*here will wrong cz constant can't reassign*/
