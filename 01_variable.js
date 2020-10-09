560cdf84c44f3b112e87062a0d3d8b897fc2e77b
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-09 13:23:11 +0700

    01_variable.js

---
+++ 01_variable.js
@@ -0,0 +1,19 @@
+// variables - store, access, modify === value
+// declare, assignment operator, assign value
+// javascript camelCase or underscore
+// case sensitive fullName and fullname
+// must start with letter, % or _
+
+// variable store
+let name = 'dncode';
+// access
+console.log(name);
+
+// just declare
+let address, zip, city;
+
+// assignment
+address = 'street heaven';
+zip = '5555';
+city = 'tokyo';
+console.log(address, zip, city);
