268f296a6c085698ab4d2814200544237c14895f
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-10 17:00:28 +0700

    function basics

---
+++ 09_function.js
@@ -0,0 +1,14 @@
+// function for reuse code and without copy paste
+// declare, and invoking
+
+// function with no parameter
+function name() {
+  console.log('Hello there');
+}
+
+// invoking with name function
+name();
+// calling 3x function
+name();
+name();
+name();
