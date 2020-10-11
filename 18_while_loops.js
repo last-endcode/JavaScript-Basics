8c8fe315f6bef4c2453069ec6708d0818ce1ac94
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-11 16:52:02 +0700

    while loops

---
+++ 18_while_loops.js
@@ -0,0 +1,39 @@
+// Loops
+// repeat run a block of code whilte condition is true
+// while loop
+// turn on autosave if vcode hangup
+
+// if u declare use const will error cz const can;t change
+// const calculate = 10;
+
+// while (calculate == 10) {
+//   console.log('calculate from : ' + calculate);
+//   calculate++;
+// }
+
+let start = 20;
+
+// while (start >= 10) {
+//   console.log('print start' + start);
+//   start++; //false and not execute
+// }
+
+// while (start >= 10) {
+//   console.log('print start' + start);
+//   start++; //will print looping cz always bigger
+// }
+
+// while (start >= 10) {
+//   console.log('print start ' + start);
+//   start--; //will print 20 ... 10 why 10, cz >=
+// }
+
+// while (start > 10) {
+//   console.log('print start ' + start);
+//   start--; //will print 20 ... 11 why 10, cz 10 > 10 is false
+// }
+
+while (start < 40) {
+  console.log(start);
+  start++; //will print 20 .. 39
+}
