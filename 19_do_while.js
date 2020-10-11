cf0ff448d340f4c310d0288c6ffe540bfa6458e9
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-11 17:03:35 +0700

    do while

---
+++ 19_do_while.js
@@ -0,0 +1,17 @@
+// check condition the last
+
+let start = 0;
+
+// do {
+//   // here will first print and
+//   // after here check condition for the last
+//   console.log(start);
+//   start++;
+// } while (start > 20);
+
+do {
+  // here will first print and
+  // after here check condition for the last
+  console.log(start);
+  start++;
+} while (start < 20);
