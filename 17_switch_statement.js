c8d945307182585c9be2a85f678df625380f12b8
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-11 16:51:46 +0700

    switch statement

---
+++ 17_switch_statement.js
@@ -0,0 +1,41 @@
+// Switch
+// dice values 1 - 6
+
+const dice = 0;
+
+switch (dice) {
+  case 0:
+    console.log('you got zero');
+    break;
+  case 1:
+    console.log('you got one');
+    break;
+  case 2:
+    console.log('you got two');
+    break;
+  case 3:
+    console.log('you got three');
+    break;
+  case 4:
+    console.log('you got four');
+    break;
+  case 5:
+    console.log('you got five');
+    break;
+  // will print if dice = 0;
+  default:
+    console.log('not in option');
+}
+
+// all if
+// const dice = 0;
+// if (dice === 1) {
+//   console.log('you got one');
+// }
+// if (dice === 2) {
+//   console.log('you got two');
+// }
+
+// if (dice < 1 || dice > 6) {
+//   console.log("you didn't roll the dice");
+// }
