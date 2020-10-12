3c5daf0a5f8db6642648042baee426df75572cc6
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-12 10:17:55 +0700

    template literal

---
+++ 22_template_literal.js
@@ -0,0 +1,9 @@
+// template literal ` `
+// insert expression ${value}
+
+let name = 'john';
+let age = 25;
+
+const result = `Today ${name} , already ${age} years old
+and here u can calculate too 4 x 5 is : ${4 * 5}`;
+console.log(result);
