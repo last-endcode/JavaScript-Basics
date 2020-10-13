44e3e9abd5e10e8b3eb492b1c5c57070cdbb04a1
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-13 19:31:17 +0700

    Array Loop

--- 24_array_loop.js
+++ 24_array_loop.js
@@ -1,17 +1,10 @@
-// Array Loops
+// Array for Loop
 
-let memberShp = ['lutfi', 'Zoro', 'Nami'];
-let lastName = ['SHP Pirates'];
-let emptyString = [];
+let name = ['john', 'peter', 'kane'];
+let lastName = 'Doe';
+let emptyArray = [];
 
-for (let i = 0; i < memberShp.length; i++) {
-  // //make sure index 0 1 2 same with array memberShp
-  // console.log(i);
-  // // for acces items array
-  // console.log(memberShp[i]);
-  // for add array
-  const callingName = `${memberShp[i]} ${lastName}`;
-  emptyString.push(`${callingName}`);
+for (let i = 0; i < name.length; i++) {
+  const fullName = `${name[i]} ${lastName}`;
+  console.log(fullName.toUpperCase());
 }
-
-console.log(emptyString);
