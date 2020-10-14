c3186fccbe1c22031fbb984dff60c9e23b2c2ba9
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-14 10:27:29 +0700

    ternary operator

---
+++ 29_ternary_operator.js
@@ -0,0 +1,18 @@
+// ternary operator
+//  condition ? (runs if true) : (runs if false)
+
+const values = 10 > 5;
+
+//values ? is condition
+values
+  ? console.log('10 bigger than five')
+  : console.log('print if this false');
+
+// // example use if else
+// const values = 10 > 5;
+// if (values) {
+//   console.log('10 bigger than five');
+// } else {
+//   console.log('false statement');
+// }
+// // output: 10 bigger than five
