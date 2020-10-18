83be1154ddb09714e71cf257755bc6daf8f3cf91
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-18 18:52:33 +0700

    math

---
+++ 40_math.js
@@ -0,0 +1,20 @@
+// math
+
+// const result = Math.PI;
+// console.log(result); //3.14
+
+// const result = Math.random() * 10;
+// console.log(result); //here he will random values
+
+// const result = Math.floor(4.555555555);
+// console.log(result); // result 4
+
+// const result = Math.ceil(4.5555);
+// console.log(result); //result 5
+
+// const result = Math.ceil(Math.random() * 10);
+// console.log(result); //here ceil never 0, if u have floor please add + 1
+
+// min max
+const result = Math.max(1, 8, 0, 9);
+console.log(result);
